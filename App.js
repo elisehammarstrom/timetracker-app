a201543b1089c08b9351491d70b23ce2e594baeb
ninaperssoon <ninap21@hotmail.com>
2023-04-03 15:04:31 +0200

    lägger till style i app.js

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import { SafeAreaView, StyleSheet, Text, View, Platform} from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Text>Hello World</Text>
-      <Text>Nina was here</Text>
+      <Text>Nina was here råtta</Text>
+      
       <Text>Platform: {Platform.OS === 'ios' ? 'ios' : 'android'}</Text>
       <StatusBar style='dark'/>
     </SafeAreaView>
@@ -13,3 +14,11 @@ export default function App() {
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
