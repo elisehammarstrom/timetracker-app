0cfd60babb8c27517d40b921d06d0ad8daaaafd6
Katariina Blom <katariinablom@student-236-96.eduroam.uu.se>
2023-04-05 11:55:43 +0200

    timer

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,10 +1,27 @@
 import React from "react";
 import { View, Text } from 'react-native';
+import CustomButton from '../../components/CustomButton';
+import { useNavigation } from '@react-navigation/native';
+
+
+
 
 const HomeScreen = () => {
+
+    const onTimerPressed = () => {
+        navigation.navigate('Timer');
+    };
+    
+    const navigation = useNavigation();
+    
     return (
         <View>
             <Text>Home, sweet home</Text>
+            <CustomButton 
+                text="Go to timer" 
+                onPress={onTimerPressed}
+            />
+
         </View>
     )
 }
