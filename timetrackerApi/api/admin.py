d8f88f8b387fdac6f9d7f6d33982fba5e2937b18
elisehammarstrom <32701757+elisehammarstrom@users.noreply.github.com>
2023-04-11 14:43:59 +0200

    skapat Programmes

--- timetrackerApi/api/admin.py
+++ timetrackerApi/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Course, CourseEvaluation
+from .models import Course, CourseEvaluation, Programme
 
 # Register your models here.
 
 admin.site.register(Course)
 admin.site.register(CourseEvaluation)
+admin.site.register(Programme)
