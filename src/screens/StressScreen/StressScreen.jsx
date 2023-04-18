6df3fae6f22bbee62f2cb69529772d69f9115f7f
Lovisa123 <lovisanilsson@student-229-67.eduroam.uu.se>
2023-04-18 09:34:46 +0200

    show calender

--- src/screens/StressScreen/StressScreen.jsx
+++ src/screens/StressScreen/StressScreen.jsx
@@ -1,7 +1,7 @@
 
 import { useNavigation } from '@react-navigation/native';
 import React, {useState, useRef, useEffect} from 'react';
-import {Text, View, TextInput, StyleSheet, FlatList, Image,TouchableOpacity,Dimensions} from 'react-native';
+import {Text, View, TextInput, StyleSheet, FlatList, Image,TouchableOpacity,Dimensions,Button} from 'react-native';
 import ButtonMenu from '../../components/ButtonMenu';
 import { SelectList } from 'react-native-dropdown-select-list'
 import DatePicker from 'react-native-modern-datepicker'
@@ -11,43 +11,78 @@ import Två from '../../../assets/2.png'
 import Tre from '../../../assets/3.png'
 import Fyra from '../../../assets/4.png'
 import Fem from '../../../assets/5.png'
+import { StatusBar } from 'expo-status-bar';
 
 const StressScreen = () => {  
-
+    
     const [selectedDate, setSelectedDate] = useState('');
     const [selected, setSelected] = useState("");
-      
+    const [isShowingImage, setShowingImage] = React.useState(false)
+    const [isShowingButton, setButton] = React.useState(true)
+   
     const data = [
     {key:'1', value:'Meknik'},
     {key:'2', value:'Reglerteknik'},
     {key:'3', value:'Envariabelanalys'},
-    ]
-    
+ ]
+ 
+ 
     return (
 
         <View style={styles.container}>
 
-            <View style={styles.selectListContainer}>
+    <View style={styles.selectListContainer}>
 
-                <SelectList
-                    dropdownTextStyles={styles.selectList}
-                    inputStyles={styles.selectList}
-                    boxStyles={styles.boxStyles}
-                    setSelected={(val) => setSelected(val)}
-                    data={data}
-                    save="value"
-                    search={false}
-                    placeholder='Choose course to track stresslevel'
-                />
+<SelectList
+    dropdownTextStyles={styles.selectList}
+    inputStyles={styles.selectList}
+    boxStyles={styles.boxStyles}
+    setSelected={(val) => setSelected(val)}
+    data={data}
+    save="value"
+    search={false}
+    placeholder='Choose course to track stresslevel'
+/>
 
-            </View>
+</View>
 
+           
+{
+        isShowingImage ?
+        (
+            <View  style={styles.dateButton}> 
+            <DatePicker
+            onSelectedChange={date => setSelectedDate(date)}
+            current="2023-04-17"
+            selected="2023-04-17"
+             mode = 'calendar'
+            style={{ borderRadius: 5, height: 250, width: 250 , padding:10, margin: 50 }}
+            
+            
+        /> 
+        <CustomButton 
+            text="Hide calendar"
+            onPress={() => setShowingImage(false)}
+
+
+            />
+             </View>
+        
+       
+         ) : (
             <View style={styles.dateButton}>
 
-                <CustomButton
-                text="Select date"
-                />
-            </View>
+            <CustomButton
+            text="Select date"
+            onPress={() => setShowingImage(true)}
+
+
+            />
+        </View>
+         )}
+      
+
+            
          
         <TouchableOpacity activeOpacity={0.5}>
             <Image 
@@ -93,14 +128,8 @@ const StressScreen = () => {
             <Text style={styles.text}> Stressfree day </Text>
         </TouchableOpacity>
 
-             {/* <DatePicker
-                    onSelectedChange={date => setSelectedDate(date)}
-                    current="2023-04-17"
-                    selected="2023-04-17"
-                     mode = 'calendar'
-                    style={{ borderRadius: 5, height: 250, width: 250 , padding:10, margin: 50 }}
-                />
-             */}
+              
+             
 
         <CustomButton
             text="Submit"
