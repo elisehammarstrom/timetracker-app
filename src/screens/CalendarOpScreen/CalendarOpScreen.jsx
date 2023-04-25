70e2ee2262e8b71501df61a4147ba497c17bb306
Lovisa123 <lovisanilsson@student-238-198.eduroam.uu.se>
2023-04-25 10:28:50 +0200

    calendarScreen

---
+++ src/screens/CalendarOpScreen/CalendarOpScreen.jsx
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+
+
+
+
+
+
+const CalendarOpScreen = () => {
+
+
+
+
+   
+    
+    return (
+<View> 
+
+
+
+
+        <Text> hej </Text>
+
+        </View>
+    
+    );
+};
+
+
+
+
+
+   
+   
+
+
+
+export default CalendarOpScreen;
