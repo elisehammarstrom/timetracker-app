f4d497bb0a4405d9587864636f17b0d598822e1a
Lovisa123 <lovisanilsson@student-238-198.eduroam.uu.se>
2023-04-25 16:06:59 +0200

    schema

---
+++ src/components/LineV/LineV.jsx
@@ -0,0 +1,20 @@
+import {  View } from 'react-native';
+import React , {useState} from 'react';
+
+const LineV = () => {
+
+    
+    return (
+<View style = { {  height: '100%',
+        width: 1,
+        color: 'white',} }>
+       
+    
+</View>
+    
+    );
+};
+
+
+
+export default LineV;
