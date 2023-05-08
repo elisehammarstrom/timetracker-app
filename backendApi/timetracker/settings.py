86d3dd1f38947baff590fe68de35aed47ea6cdae
elisehammarstrom <32701757+elisehammarstrom@users.noreply.github.com>
2023-05-08 14:17:44 +0200

    login fixed

--- backendApi/timetracker/settings.py
+++ backendApi/timetracker/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
 }
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication", 
+        #"rest_framework.authentication.SessionAuthentication", 
         "rest_framework.authentication.TokenAuthentication", 
     
     ),
