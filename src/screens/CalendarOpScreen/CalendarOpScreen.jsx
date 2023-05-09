e80a6d1063cd3cb0a0ed9b5b46506af9eed7ed11
Katariina Blom <katariina@bloms.net>
2023-05-09 09:56:56 +0200

    calendar screen date

--- src/screens/CalendarOpScreen/CalendarOpScreen.jsx
+++ src/screens/CalendarOpScreen/CalendarOpScreen.jsx
@@ -1,8 +1,5 @@
-import { StyleSheet, Text, View , SafeAreaView, FlatList, TouchableHighlight, Image} from 'react-native';
+import { StyleSheet, Text, View ,TouchableOpacity, Image} from 'react-native';
 import React , {useState} from 'react';
-import WeekCalendar from '../../components/WeekCalendar';
-import Line from '../../components/Line';
-import LineV from '../../components/LineV';
 import { useNavigation } from '@react-navigation/native';
 import CloseIcon from '../../../assets/close.png'
 
@@ -10,7 +7,6 @@ import CloseIcon from '../../../assets/close.png'
 
 
 const CalendarOpScreen = () => {
-    const [date, setDate] = useState(new Date());
 
     const navigation = useNavigation();
 
@@ -19,48 +15,36 @@ const CalendarOpScreen = () => {
     const onClosedPress = () => {
         navigation.navigate('Home', {options: courses})
       }
+
+      var todaysDate = new Date().getDate();
+      var monthNumber = new Date().getMonth() + 1;
+
+
+      function getMonthName(monthNumber) {
+        const date = new Date();
+        date.setMonth(monthNumber - 1);
+
+        return date.toLocaleString('en-US', { month: 'long' });
+      }
+
+      var showMonth = getMonthName(monthNumber)
  
   return (
     <View style={styles.container}> 
-
-         <TouchableHighlight style={styles.closeContainer} onPress={onClosedPress} >
+    <View style={styles.topContainer}>
+         <TouchableOpacity activeOpacity={0.5} style={styles.close} onPress={onClosedPress} >
             <Image 
               source={CloseIcon} 
               style={[ {height: 100 * 0.3},{width: 100*0.3}]} 
               resizeMode="contain"
             />
-        </TouchableHighlight>
+        </TouchableOpacity>
 
-    
-      <View style={styles.week}> 
- 
-       <WeekCalendar date={date} onChange={(newDate) => setDate(newDate)} />
-        <LineV> </LineV>
-      </View>
 
-{/*       OBS!!!!! Flatlisten funkar inte med iOS */}
+          <Text style = {styles.text}>{todaysDate} {showMonth}</Text>
+          </View>
 
-      <FlatList
-        data={[
-          {key: '07'},
-          {key: '08'},
-          {key: '09'},
-          {key: '10'},
-          {key: '11'},
-          {key: '12'},
-          {key: '13'},
-          {key: '14'},
-          {key: '15'},
-          {key: '16'},
-          {key: '17'},
-          {key: '18'},
-          {key: '19'},
-          {key: '20'},
-          {key: '21'},
-          {key: '22'},
-        ]}
-        renderItem={({item}) =>  <View>  <Line ></Line> <Text style={styles.item}>{item.key}</Text> </View>}
-      />
+
         </View>
     
   );
@@ -77,30 +61,18 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         
     },
-    item: {
-        padding: 10,
-        fontSize: 18,
-        height: 44,
-        color:'white',
+      text: {
+        color: 'white',
+        fontSize: 30,
+        flex: 5
       },
-      week:{
-        width:'95%',
-        paddingLeft:40,
-      },
-      verticleLine: {
-        height:'100%',
-        width: 1.5,
-        backgroundColor: '#909090',
+      topContainer: {
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        padding: 10
       },
       close: {
-        color: 'white',
-        fontSize: 20,
-        padding: 10,
-        alignItems: 'flex-start'
-      },
-      closeContainer: {
-        alignItems: 'flex-start',
-        padding: 10,
-      },
+        flex: 3
+      }
    
 })
