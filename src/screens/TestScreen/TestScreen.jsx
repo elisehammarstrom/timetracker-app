79e250ccc3b61fd8932f524c397635987c025ae3
Katariina Blom <katariina@bloms.net>
2023-05-11 09:46:46 +0200

    dropdown menu for assignments

--- src/screens/TestScreen/TestScreen.jsx
+++ src/screens/TestScreen/TestScreen.jsx
@@ -1,10 +1,25 @@
 
 import {useState, useEffect} from 'react'
-import {FlatList, View, Text} from 'react-native'
+import {FlatList, View, Text, StyleSheet} from 'react-native'
 import React from 'react'
+import {Picker} from '@react-native-picker/picker';
+import DropDown from '../../components/DropDown';
 
 export default function TestScreen() {
 const [courses, setCourses] = useState ( [ ] ) ;
+const [selectedItem, setSelectedItem] = useState(null);
+
+/* const [pickerValue, setPickerValue] = useState('råtta')
+ */
+let fruits = [
+    {id: 1, name: 'mango'},
+    {id: 2, name: 'banana'},
+    {id: 3, name: 'cherry'}
+]
+
+const onSelect = (item) => {
+    setSelectedItem(item)
+}
 
 
 useEffect ( () => {
@@ -22,18 +37,46 @@ useEffect ( () => {
 }, [] );
 
 
+
 return (
 <View>
-<FlatList
+
+    <DropDown
+        value={selectedItem}
+        data={fruits}
+        onSelect={onSelect}
+    />
+
+            {/* <Picker
+              style={styles.picker}
+              selectedValue = {pickerValue}
+              onValueChange = {(itemValue) => (setPickerValue(itemValue))}
+              >
+                <Picker.Item label ='Råtta' value='råtta'/>
+                <Picker.Item label ='Åsna' value='åsna'/>
+                <Picker.Item label ='Berit' value='berit'/>
+
+              </Picker> */}
+{/* <FlatList
 data={courses}
 renderItem={ ( {item} ) => (
 <Text key={item.id}> {item.courseTitle} </Text>
 ) }
 
-/>
+/> */}
 </View>
 
 );}
+
+const styles = StyleSheet.create({
+
+picker: {
+    width: 200,
+    height: 1,
+    backgroundColor: 'red'
+
+  }, 
+})
 
  
 
