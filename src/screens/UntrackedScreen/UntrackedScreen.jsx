ec74410fcf7faf4f9aadb2a247780681676675bf
Katariina Blom <katariina@bloms.net>
2023-05-12 15:14:24 +0200

    dropdowns fixed and course evaluation screen style

--- src/screens/UntrackedScreen/UntrackedScreen.jsx
+++ src/screens/UntrackedScreen/UntrackedScreen.jsx
@@ -1,6 +1,6 @@
 
-import React, {useState} from 'react';
-import {Text, View,Dimensions,StyleSheet, Button, TouchableHighlight, TouchableOpacity, Image} from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, Dimensions, StyleSheet, Button, TouchableHighlight, TouchableOpacity, Image } from 'react-native';
 import { SelectList } from 'react-native-dropdown-select-list'
 import ButtonMenu from '../../components/ButtonMenu';
 import WeekCalendar from '../../components/WeekCalendar';
@@ -13,10 +13,10 @@ import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
 
 
-const UntrackedScreen = ({route}) => {
-    const {courses} = route.params;
-    const {token} = route.params;
-    const {courseIDs} = route.params;
+const UntrackedScreen = ({ route }) => {
+    const { courses } = route.params;
+    const { token } = route.params;
+    const { courseIDs } = route.params;
 
     const [selectedCourse, setSelectedCourse] = React.useState("");
     const [selectedHour, setSelectedHour] = React.useState("00");
@@ -33,46 +33,47 @@ const UntrackedScreen = ({route}) => {
 
 
     var day = new Date().getDate();
-    var month = new Date().getMonth()+1;
+    var month = new Date().getMonth() + 1;
     var year = new Date().getFullYear();
 
     const [thisDate, setThisDate] = useState('');
 
     if (thisDate.length < 1) {
-    if (`${day}`.length === 1 & `${month}`.length === 1) {
-        setThisDate(year + '-0' + month + '-0' + day)
-    }
-    else if (`${day}`.length === 1) {
-        setThisDate(year + '-' + month + '-0' + day)
-    }
-    else if (`${month}`.length === 1) {
-        setThisDate(year + '-0' + month + '-' + day)
-    }
-    else {
-        setThisDate(year + '-' + month + '-' + day)
+        if (`${day}`.length === 1 & `${month}`.length === 1) {
+            setThisDate(year + '-0' + month + '-0' + day)
+        }
+        else if (`${day}`.length === 1) {
+            setThisDate(year + '-' + month + '-0' + day)
+        }
+        else if (`${month}`.length === 1) {
+            setThisDate(year + '-0' + month + '-' + day)
+        }
+        else {
+            setThisDate(year + '-' + month + '-' + day)
 
-    }}  
+        }
+    }
 
 
     const hourData = [
-        {key:'1', value:'00'},
-        {key:'2', value:'01'},
-        {key:'3', value:'02'},
-        {key:'4', value:'03'},
-        {key:'5', value:'04'},
-        {key:'6', value:'05'},
-        {key:'7', value:'06'},
-        {key:'8', value:'07'},
-        {key:'9', value:'08'},
-        {key:'10', value:'09'},
-        {key:'11', value:'10'},
+        { key: '1', value: '00' },
+        { key: '2', value: '01' },
+        { key: '3', value: '02' },
+        { key: '4', value: '03' },
+        { key: '5', value: '04' },
+        { key: '6', value: '05' },
+        { key: '7', value: '06' },
+        { key: '8', value: '07' },
+        { key: '9', value: '08' },
+        { key: '10', value: '09' },
+        { key: '11', value: '10' },
     ]
 
     const minuteData = [
-        {key:'1', value:'00'},
-        {key:'2', value:'15'},
-        {key:'3', value:'30'},
-        {key:'4', value:'45'},
+        { key: '1', value: '00' },
+        { key: '2', value: '15' },
+        { key: '3', value: '30' },
+        { key: '4', value: '45' },
     ]
 
     const [date, setDate] = useState(new Date());
@@ -81,186 +82,186 @@ const UntrackedScreen = ({route}) => {
 
     //Change it so that it is 7 days in the past.
     var pastDate = ourDate.getDate() - 7;
-    ourDate.setDate(pastDate);      
+    ourDate.setDate(pastDate);
 
-    const onCalendarPressed = () => {       
+    const onCalendarPressed = () => {
         setDate(ourDate)
-        
-      }
 
-      const onClosedPress = () => {
-        navigation.navigate('Home', {options: courses}, {token: token})
-      }
+    }
 
-    const onCurrentDatePressed = () => {       
-    setDate(new Date())
+    const onClosedPress = () => {
+        navigation.navigate('Home', { options: courses }, { token: token })
+    }
+
+    const onCurrentDatePressed = () => {
+        setDate(new Date())
     }
 
     const onAddTimePressed = () => {
-        for (let i=0; i<courses.length; i++){
+        for (let i = 0; i < courses.length; i++) {
             if (selectedCourse === courses[i]) {
-                let time = selectedHour + ':' + selectedMinute + ':' + '00' ;
+                let time = selectedHour + ':' + selectedMinute + ':' + '00';
                 console.log("date= ", thisDate)
                 const formData = new FormData();
                 formData.append('courseID', courseIDs[i]);
                 formData.append('date', thisDate);
                 formData.append('duration', time);
-        
-                axios({
-                  method: "post",
-                  url: "http://127.0.0.1:8000/api/tracking/add_untracked_time/",
-                  data: formData,
-                  headers: {
-                    'Content-Type': 'multipart/form-data',
-                    'Authorization':`token ` + token
-                  }
-                })
-                  .then(function (response) {
-                    //handle success
-                    console.log(response.data);
-                    alert('Time tracked');
 
-                  })
-                  .catch(function (response) {
-                    //handle error
-                    console.log(response);
-                  });
-        
-        
+                axios({
+                    method: "post",
+                    url: "http://127.0.0.1:8000/api/tracking/add_untracked_time/",
+                    data: formData,
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                        'Authorization': `token ` + token
+                    }
+                })
+                    .then(function (response) {
+                        //handle success
+                        console.log(response.data);
+                        alert('Time tracked');
+
+                    })
+                    .catch(function (response) {
+                        //handle error
+                        console.log(response);
+                    });
+
+
             }
         }
-        
-        
+
+
     }
 
     return (
-    <View style={styles.container}>
-        <View style={styles.topContainer}>
-        <TouchableOpacity activeOpacity={0.5} style={styles.close} onPress={onClosedPress} >
-            <Image 
-              source={ArrowBack} 
-              style={[ {height: 100 * 0.3},{width: 100*0.3}]} 
-              resizeMode="contain"
-            />
-        </TouchableOpacity>
-            <Text style={styles.title}>Add untracked time</Text>
-        </View>
- 
-        {
-            isShowingArrow ?
-            (
-                <View style={styles.layout}>
-                    <TouchableOpacity onPress={() => {onCalendarPressed(true); setShowingArrow(false);}}> 
-                        <Image 
-                            source={LeftArrow} 
-                            style={[ {height: 100 * 0.3},{width: 100*0.3}]} 
-                            resizeMode="contain"
-                        />
-                    </TouchableOpacity>
+        <View style={styles.container}>
+            <View style={styles.topContainer}>
+                <TouchableOpacity activeOpacity={0.5} style={styles.close} onPress={onClosedPress} >
+                    <Image
+                        source={ArrowBack}
+                        style={[{ height: 100 * 0.3 }, { width: 100 * 0.3 }]}
+                        resizeMode="contain"
+                    />
+                </TouchableOpacity>
+                <Text style={styles.title}>Add untracked time</Text>
+            </View>
 
-                    <View style={styles.calendar}>
-                    <WeekCalendar date={date} onChange={(newDate) => setDate(newDate)} />
-                    </View>
-                    <View>
-                        <Image 
-                            source={Invisible} 
-                            style={[ {height: 100 * 0.3},{width: 100*0.3}]} 
-                            resizeMode="contain"
-                        />
-                    </View>
-                </View>
-            ) 
-            : 
-            (
-                <View style={styles.layout}>
-                    <View>
-                        <Image 
-                            source={Invisible} 
-                            style={[ {height: 100 * 0.3},{width: 100*0.3}]} 
-                            resizeMode="contain"
-                        />
-                    </View>
-                    <View style={{paddingHorizontal: 10}}>
-                    <WeekCalendar date={date} onChange={(newDate) => setDate(newDate)} />
-                    </View>
-                    
-                    <TouchableOpacity onPress={() => {onCurrentDatePressed(true); setShowingArrow(true);}}> 
-                        <Image 
-                            source={RightArrow} 
-                            style={[ {height: 100 * 0.3},{width: 100*0.3}]} 
-                            resizeMode="contain"
-                        />
-                    </TouchableOpacity>
-                </View>
-            )
-        } 
-        
+            {
+                isShowingArrow ?
+                    (
+                        <View style={styles.layout}>
+                            <TouchableOpacity onPress={() => { onCalendarPressed(true); setShowingArrow(false); }}>
+                                <Image
+                                    source={LeftArrow}
+                                    style={[{ height: 100 * 0.3 }, { width: 100 * 0.3 }]}
+                                    resizeMode="contain"
+                                />
+                            </TouchableOpacity>
 
-        <View style={styles.selectListContainer}>
-            <SelectList
-                dropdownTextStyles={styles.selectList}
-                inputStyles={styles.selectList}
-                boxStyles={styles.boxStyles}
-                setSelected={(val) => setSelectedCourse(val)}
-                data={courses}
-                save="value"
-                search={false}
-                placeholder='Choose course'
-                dropdownStyles={styles.dropDown}
-            />
-        </View>
+                            <View style={styles.calendar}>
+                                <WeekCalendar date={date} onChange={(newDate) => setDate(newDate)} />
+                            </View>
+                            <View>
+                                <Image
+                                    source={Invisible}
+                                    style={[{ height: 100 * 0.3 }, { width: 100 * 0.3 }]}
+                                    resizeMode="contain"
+                                />
+                            </View>
+                        </View>
+                    )
+                    :
+                    (
+                        <View style={styles.layout}>
+                            <View>
+                                <Image
+                                    source={Invisible}
+                                    style={[{ height: 100 * 0.3 }, { width: 100 * 0.3 }]}
+                                    resizeMode="contain"
+                                />
+                            </View>
+                            <View style={{ paddingHorizontal: 10 }}>
+                                <WeekCalendar date={date} onChange={(newDate) => setDate(newDate)} />
+                            </View>
 
-        <View style={styles.timeLayout}>
-            <View style={styles.selectListTimeContainer}>
-                <Text style={styles.addTime}>Add time:</Text>
-        
+                            <TouchableOpacity onPress={() => { onCurrentDatePressed(true); setShowingArrow(true); }}>
+                                <Image
+                                    source={RightArrow}
+                                    style={[{ height: 100 * 0.3 }, { width: 100 * 0.3 }]}
+                                    resizeMode="contain"
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    )
+            }
+
+
+            <View style={styles.selectListContainer}>
                 <SelectList
                     dropdownTextStyles={styles.selectList}
                     inputStyles={styles.selectList}
-                    boxStyles={styles.boxTimeStyles}
-                    setSelected={(val) => setSelectedHour(val)}
-                    data={hourData}
+                    boxStyles={styles.boxStyles}
+                    setSelected={(val) => setSelectedCourse(val)}
+                    data={courses}
                     save="value"
                     search={false}
-                    placeholder='00'
-                    dropdownStyles={{width: 0.18 * Dimensions.get('window').width, borderColor: '#313131'}}
-                    
-                />
-                <Text style={{color: 'white', fontSize: 30}}>:</Text>
-
-                <SelectList
-                    dropdownTextStyles={styles.selectList}
-                    inputStyles={styles.selectList}
-                    boxStyles={styles.boxTimeStyles}
-                    setSelected={(val) => setSelectedMinute(val)}
-                    data={minuteData}
-                    save="value"
-                    search={false}
-                    placeholder='00'
-                    dropdownStyles={{width: 0.18 * Dimensions.get('window').width, borderColor: '#313131'}}
+                    placeholder='Choose course'
+                    dropdownStyles={styles.dropDown}
                 />
             </View>
+
+            <View style={styles.timeLayout}>
+                <View style={styles.selectListTimeContainer}>
+                    <Text style={styles.addTime}>Add time:</Text>
+
+                    <SelectList
+                        dropdownTextStyles={styles.selectList}
+                        inputStyles={styles.selectList}
+                        boxStyles={styles.boxTimeStyles}
+                        setSelected={(val) => setSelectedHour(val)}
+                        data={hourData}
+                        save="value"
+                        search={false}
+                        placeholder='00'
+                        dropdownStyles={{ width: 0.18 * Dimensions.get('window').width, borderColor: '#313131' }}
+
+                    />
+                    <Text style={{ color: 'white', fontSize: 30 }}>:</Text>
+
+                    <SelectList
+                        dropdownTextStyles={styles.selectList}
+                        inputStyles={styles.selectList}
+                        boxStyles={styles.boxTimeStyles}
+                        setSelected={(val) => setSelectedMinute(val)}
+                        data={minuteData}
+                        save="value"
+                        search={false}
+                        placeholder='00'
+                        dropdownStyles={{ width: 0.18 * Dimensions.get('window').width, borderColor: '#313131' }}
+                    />
+                </View>
+            </View>
+
+            <View style={styles.customButtonContainer}>
+                <CustomButton
+                    text="Add time"
+                    onPress={onAddTimePressed}
+                />
+            </View>
+            <View>
+
+            </View>
+            <View>
+                <ButtonMenu
+                    screen='timeTracking'
+                    token={token}
+                />
+            </View>
+
         </View>
 
-        <View style={styles.customButtonContainer}>
-            <CustomButton
-                text="Add time"
-                onPress={onAddTimePressed}
-            />
-        </View>
-        <View>
-
-        </View>
-        <View>
-            <ButtonMenu 
-                screen='timeTracking'
-                token={token}
-            />
-        </View>
-        
-    </View>
-        
-)
+    )
 }
 
 
@@ -274,14 +275,13 @@ const styles = StyleSheet.create({
     selectListContainer: {
         justifyContent: 'center',
         marginTop: '3%',
-        flexDirection: 'row',    
+        flexDirection: 'row',
     },
     selectListTimeContainer: {
         justifyContent: 'center',
         flexDirection: 'row',
         borderRadius: 20,
         borderColor: 'white',
-
         borderWidth: 0.5
 
     },
@@ -310,21 +310,21 @@ const styles = StyleSheet.create({
     },
     dateButton: {
         marginRight: '2%',
-        width:100, 
+        width: 100,
     },
     logo: {
         width: '70%',
         maxWidth: 300,
-        maxHeight: 200,  
-        flexDirection:'row',
+        maxHeight: 200,
+        flexDirection: 'row',
     },
-    text:{
-        marginLeft:220,
-        flexDirection:'row',
+    text: {
+        marginLeft: 220,
+        flexDirection: 'row',
         position: "absolute",
-        marginTop:23,
-        fontsize:20,
-        fontWeight:'bold',
+        marginTop: 23,
+        fontsize: 20,
+        fontWeight: 'bold',
         color: 'white',
     },
     layout: {
@@ -345,8 +345,8 @@ const styles = StyleSheet.create({
     customButtonContainer: {
         paddingHorizontal: 50,
         marginTop: 30,
-      },
-      title: {
+    },
+    title: {
         fontSize: 24,
         fontWeight: 'bold',
         color: '#EFEFEF',
