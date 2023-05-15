11e9e33f604962f76b297986b92a9bb81f13bcb5
Katariina Blom <katariina@bloms.net>
2023-05-15 17:46:40 +0200

    scroll view your reports

--- src/screens/YourReportsScreen/YourReportsScreen.jsx
+++ src/screens/YourReportsScreen/YourReportsScreen.jsx
@@ -10,7 +10,6 @@ import Två from '../../../assets/2.png'
 import Tre from '../../../assets/3.png'
 import Fyra from '../../../assets/4.png'
 import Fem from '../../../assets/5.png'
-import { ConsoleSqlOutlined } from '@ant-design/icons';
 
 
 const YourReportsScreen = ({route}) => {
@@ -278,6 +277,9 @@ const YourReportsScreen = ({route}) => {
                 <Text style={styles.dataTextCourse}>Course</Text>
                 <Text style={styles.dataTextStress}>Stress</Text>
               </View>
+              <ScrollView style={styles.scrollView}>
+                <View style={styles.center}>
+        
              
               {courses.map((course,i) => (
                
@@ -298,8 +300,10 @@ const YourReportsScreen = ({route}) => {
                 </TouchableOpacity> 
    
               ))}
-               
+              </View>
+               </ScrollView>
             </View>
+            
 
             <View>
               <ButtonMenu
@@ -368,6 +372,14 @@ const styles=StyleSheet.create({
     justifyContent: 'space-between',
     width: '85%'
   },
+  scrollView: {
+    width: Dimensions.get('window').width,
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 40
+  }
 })
 
 export default YourReportsScreen;
