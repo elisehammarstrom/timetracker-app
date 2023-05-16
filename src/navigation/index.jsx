1688491b8392e3ceb53db2d51383c0229d18335b
ninaperssoon <ninap21@hotmail.com>
2023-05-16 11:10:48 +0200

    removed forgot password and related screens

--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -3,9 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import SignInScreen from '../screens/SignInScreen';
 import SignUpScreen from '../screens/SignUpScreen/SignUpScreen';
-import ConfirmEmailScreen from '../screens/ConfirmEmailScreen';
-import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
-import NewPasswordScreen from '../screens/NewPasswordScreen';
 import HomeScreen from '../screens/HomeScreen';
 import TimerScreen from '../screens/TimerScreen';
 import CourseScreen from '../screens/CourseScreen';
@@ -35,9 +32,6 @@ const Navigation = () => {
                 
                 <Stack.Screen name="SignIn" component={SignInScreen} />
                 <Stack.Screen name="SignUp" component={SignUpScreen} />
-                <Stack.Screen name="ConfirmEmail" component={ConfirmEmailScreen} />
-                <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
-                <Stack.Screen name="NewPassword" component={NewPasswordScreen} />
                 <Stack.Screen name="Profile" component={ProfileScreen} />
 
                 <Stack.Screen name="StartCourses" component={CourseScreen}/>
