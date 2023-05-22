618f7b534573b997e68484c9ecca4e0be34917ee
Katariina Blom <katariina@bloms.net>
2023-05-22 11:49:29 +0200

    font is now trebuchet ms

--- src/components/Text/Text.jsx
+++ src/components/Text/Text.jsx
@@ -2,4 +2,4 @@ import {Text} from 'react-native';
 
 
 //Change font for the text
-export default props => <Text {...props} style={[{fontFamily: 'Avenir'}, props.style]}>{props.children}</Text>
+export default props => <Text {...props} style={[{fontFamily: 'Trebuchet MS'}, props.style]}>{props.children}</Text>
